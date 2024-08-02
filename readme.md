7ad826e5c0f86c71eeb9ce585dd18dd957c01e10
Sourabh Singh <sourabhsingh01998@gmail.com>
2024-08-02 22:00:41 +0530

    Error Handling Basic - Using ControllerAdvice and ExpceptionHandler

--- readme.md
+++ readme.md
@@ -2,20 +2,21 @@
 
 ---
 [x] Setup Spring Boot Project and Install Basic Library <br>
-[x] FakeStore Calling Third Party API's.
-[x] Rest Template, RestController and route Mapping etc.
+[x] FakeStore Calling Third Party API's. <br>
+[x] Rest Template, RestController and route Mapping etc. <br>
 
 ### Class Second - Jul 30
 
 ---
-[x] ORM , JPA vs JDBC vs Hibrnate
-[x] Repository Pattern
+[x] Controller adviser
+[x] ORM , JPA vs JDBC vs Hibrnate <br>
+[x] Repository Pattern <br> 
 [x] Modals in Codebase - Base Modal, isDeleted (soft delete)
 
 ### Class third - Aug 1
 
 ---
 
-[x] Coding - Modals, Services - Connecting to the database (Mysql)
-[x] How to represent Cardianalities
+[x] Coding - Modals, Services - Connecting to the database (Mysql) <br>
+[x] How to represent Cardianalities <br>
 [x] JPA Query Methods (Repositories)
--- src/main/java/com/scaler/productservice/advicers/ExceptionAdviser.java
+++ src/main/java/com/scaler/productservice/advicers/ExceptionAdviser.java
@@ -1,4 +1,40 @@
 package com.scaler.productservice.advicers;
 
+import com.scaler.productservice.dtos.ErrorResponseDto;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.net.NoRouteToHostException;
+
+@ControllerAdvice
+@RestController
 public class ExceptionAdviser {
+    @ExceptionHandler(RuntimeException.class)
+    public ErrorResponseDto handleRuntimeException(RuntimeException e) {
+        ErrorResponseDto dto = new ErrorResponseDto();
+        dto.setStatus("ERROR");
+        dto.setMessage(e.getMessage());
+        return dto;
+    }
+
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class, NoResourceFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDto handleNoHandlerException(){
+        ErrorResponseDto error = new ErrorResponseDto();
+        error.setMessage("Route Not Found");
+        error.setStatus("Error");
+        return error;
+    }
+    @ExceptionHandler(Exception.class)
+    public String handleException() {
+        return "something went wrong";
+    }
 }
